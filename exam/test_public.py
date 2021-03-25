4e452dcc081560ebe2550dabf75499743d504c4d
Drozdova-Daria <60818854+Drozdova-Daria@users.noreply.github.com>
2021-03-25 14:42:08 +0500

    Update test_public.py

--- exam/test_public.py
+++ exam/test_public.py
@@ -13,11 +13,12 @@ class Case:
 
 
 TEST_CASES = [
-    Case(name='basis', expected='Hello World')
+    Case(name='basis', expected=['Ten', 'little', 'Crip', 'niggaz', 'runnin', 'outside\n', 'All', 'from', 'the', 'turf', 'and', 'they', 'bangin', 'out', 'lives\n', 'Far', 'from', 'a', 'scrub', 'cuzz', 'from', 'the', 'Eastside\n', 'Where', 'they', "don't", 'die', 'they', 'just', 'multiply\n', 'So..', "don't", 'give', 'them', 'niggaz', 'a', 'reason\n', 'To', 'turn', 'it', 'into', 'spray', 'season\n', "Don't", 'give', 'them', 'niggaz', 'a', 'reason\n', 'Because', "they'll", 'turn', 'it', 'into', 'Crip', 'season\n', 'The', "gangsta's", 'back,', 'the', 'bank', 'is', 'fat\n']
+)
 ]
 
 
 @pytest.mark.parametrize('test_case', TEST_CASES, ids=str)
 def test_hello_world(test_case: Case) -> None:
-    answer = hello()
-assert answer == test_case.expected
+    answer = words('test.txt')
+    assert answer == test_case.expected
